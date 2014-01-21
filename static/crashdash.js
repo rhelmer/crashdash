0c60f86a3b97b1506d4aab1616660ab7a07f2598
Robert Helmer <rhelmer@rhelmer.org>
2014-01-21 14:49:49 -0800

    style

--- static/crashdash.js
+++ static/crashdash.js
@@ -1,6 +1,23 @@
 $(function(){
-    var api_url = 'http://localhost:5000/api/';
-    $.get(api_url + 'CrashTrends/?end_date=2014-01-21&product=Firefox&start_date=2014-01-19&version=29.0a1', function(payload) {
-        console.log(payload);
+    var api_url = '/api/';
+    $.getJSON(api_url + 'CurrentVersions', function(payload) {
+        var featured = {};
+        $.each(payload, function(idx, release) {
+            if (release.featured) {
+                featured[release.product] = release.version;
+            }
+        });
+        $.each(featured, function(product, version) {
+            $('#product-list')
+                .append('<a href="' + product + '"><li>' + 
+                        product + '</li></a>');
+        });
     });
+
+    function crashTrends(product, version) {
+        $.getJSON(api_url + 'CrashTrends/?end_date=2014-01-21&product=' + product + '&start_date=2014-01-19&version=' + version, function(payload) {
+            $.each(payload.crashtrends, function(idx, trend) {
+            });
+        });
+    }
 });
