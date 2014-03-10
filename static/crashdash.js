8a47f22a82ad345591f4379bd53174eb69013180
Robert Helmer <rhelmer@rhelmer.org>
2014-03-09 21:22:52 -0700

    get basic front page going in d3

--- static/crashdash.js
+++ static/crashdash.js
@@ -6,17 +6,28 @@ $(function(){
                     'MetroFirefox': 'Firefox Metro',
                     'Thunderbird': 'Thunderbird',
                     'SeaMonkey': 'SeaMonkey'}
+
     d3.json(api_url + 'CurrentVersions', function(data) {
-        var featured = [];
+        var featured = {};
         data.forEach(function(release, i) {
             if (release.featured) {
-                featured.push(release);
+                var productName = release.product;
+                if (productName in featured) {
+                    featured[productName].push(release);
+                } else {
+                    featured[productName] = [release]
+                }
             }
         });
-        console.log('featured:', featured);
-        d3.select('product-list')
-            .data(featured)
+        d3.select('ul').selectAll('li')
+            .data(Object.keys(products))
           .enter().append('li')
-            .text(function(d) { return d; });
+            .text(function(d) {
+                return products[d];
+            })
+          .append('img')
+            .attr({
+                'src': function(d) { return ('/static/' + d + '.png'); }
+            });
     });
 });
