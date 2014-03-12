9c1af94aad20bfc2afadb61c9aace55088499bfc
Robert Helmer <rhelmer@rhelmer.org>
2014-03-11 23:19:14 -0700

    add explosive crash report and reformat

--- static/crashdash.js
+++ static/crashdash.js
@@ -7,11 +7,22 @@ $(function(){
                     'Thunderbird': 'Thunderbird',
                     'SeaMonkey': 'SeaMonkey'}
 
-    d3.json(api_url + 'CurrentVersions', function(data) {
+    d3.json(api_url + 'ExplosiveCrashes/', function(explosiveCrashes) {
+        var crashes = explosiveCrashes['hits'];
+        d3.select('#warnings').selectAll('div')
+            .data(crashes)
+            .enter()
+            .append('span')
+            .text(function(d) { 
+                return 'Explosive crash: ' + d;
+            })
+    });
+
+    d3.json(api_url + 'CurrentVersions', function(currentVersions) {
         var featured = {};
-        data.forEach(function(release, i) {
+        currentVersions.forEach(function(release, i) {
+            var productName = release.product;
             if (release.featured) {
-                var productName = release.product;
                 if (productName in featured) {
                     featured[productName].push(release);
                 } else {
@@ -19,15 +30,33 @@ $(function(){
                 }
             }
         });
-        d3.select('ul').selectAll('li')
-            .data(Object.keys(products))
-          .enter().append('li')
-            .text(function(d) {
-                return products[d];
-            })
-          .append('img')
-            .attr({
-                'src': function(d) { return ('/static/' + d + '.png'); }
+        Object.keys(products).forEach(function(productName) {
+            var url = api_url + 'CrashesPerAdu' + '?product=' + productName;
+            for (var i=0; i < featured[productName].length; i++) {
+                url += '&versions=' + featured[productName][i].version;
+            }
+            d3.json(url, function(crashesPerAdu) {
+                d3.select('ul').selectAll('li')
+                    .data(Object.keys(products))
+                    .enter()
+                    .append('li')
+                    .text(function(d) { return products[d]; })
+                    .append('p')
+                    .text(function(d) {
+                        versions = [];
+                        featured[d].forEach(function(release) {
+                            versions.push(release.version);
+                            var productVersion = d + ':' + release.version;
+                        });
+                        return versions;
+                    })
+                    .append('img')
+                    .attr({
+                        'src': function(d) {
+                            return ('/static/' + d + '.png');
+                        }
+                    })
             });
+        });
     });
 });
